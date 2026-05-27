199bb592af18f7f57290430c39d6f554b8c0f7fb
LMCuong2K1 <lmc22112001@gmail.com>
2026-05-28 02:32:13 +0700

    create authMiddleware

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const catchAsync = require('catch-async-wrapper-express');
+const AppError = require('../utils/AppError');
+
+class AuthMiddleware {
+    protect = catchAsync(async (req, res, next) => {
+        let token;
+        if (req.headers.authorization) {
+            if (req.headers.authorization.startsWith("Bearer")) {
+                token = req.headers.authorization.split(' ')[1];
+            }
+            else { token = req.headers.authorization };
+            const decode = jwt.verify(token, process.env.JWT_SECRET);
+            const user = await User.findById(decode.id);
+            if (!user) throw new AppError("User không tồn tại!", 401);
+            req.user = user;
+            next();
+        }
+        else throw new AppError("Chưa đăng nhập/Thiếu token!", 401);
+    });
+    admin = catchAsync((req, res, next) => {
+        if (req.user && req.user.role === "Admin") next();
+        else throw new AppError("Bạn không phải Admin", 403);
+    })
+}
+
+
+module.exports = new AuthMiddleware;
